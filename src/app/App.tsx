2de41504b83d257b9fab4c9e47a539d1ebdea5aa
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-10-13 11:18:19 +0200

    feat: add routing

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "../common/components/Navbar";
 import SearchForm from "../common/components/SearchForm";
+import Login from "../common/components/login/Login";
+import MainPage from "../common/components/mainPage/MainPage";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 function App() {
+  const [token, setToken] = useState();
+  
   return (
-    <div className="App">
-      <header className="App-header" />
-      <Navbar></Navbar>
-      <SearchForm></SearchForm>
-    </div>
+    <Routes>
+        <Route path='/' element={<MainPage />}></Route>
+        <Route path='/login' element={<Login />} />
+    </Routes>
   );
 }
 
