71e5c2067bbd928b848903897980a891c10bfebb
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-10-13 11:18:31 +0200

    feat: add new pages

---
+++ src/common/components/mainPage/MainPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+
+export default function MainPage() {
+  return (
+    <>
+      <div>hello</div>
+      <Outlet />
+    </>
+  )
+}
