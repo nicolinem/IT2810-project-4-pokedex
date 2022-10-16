cb1b3d5fdc7eea18d9e65e64e07e00470f5b5e8b
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-10-16 16:39:21 +0200

    fix: remove imports

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,9 @@
-import React, { useState } from "react";
-import Navbar from "../common/components/Navbar";
-import SearchForm from "../common/components/SearchForm";
 import Login from "../common/components/login/Login";
 import MainPage from "../common/components/mainPage/MainPage";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 
 function App() {
-  const [token, setToken] = useState();
   
   return (
     <Routes>
