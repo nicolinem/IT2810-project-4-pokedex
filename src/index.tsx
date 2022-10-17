5bce07e118f2902584769d84b60108b92ec7c181
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-10-17 08:55:36 +0200

    feat: setup database connection

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,27 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./app/App";
 import "./styles/globals.css";
+import {ApolloProvider, ApolloClient, InMemoryCache} from '@apollo/client';
+
+
+
+const client = new ApolloClient({
+  uri: "http://localhost:4001/graphql",
+  cache: new InMemoryCache(),
+});
+
+
+console.log(client)
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
       <BrowserRouter>
       <App />
       </BrowserRouter>
+    </ApolloProvider>
   </React.StrictMode>
 );
