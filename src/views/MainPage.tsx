9fcecd34e97dcfe6e8c45c1ec691585ac0166dd7
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-10-19 19:39:49 +0200

    fix: update imports

--- src/views/MainPage.tsx
+++ src/views/MainPage.tsx
@@ -1,18 +1,11 @@
-import { IsLoggedIn } from "../cache";
-import { useSignout } from "../hooks/useSignOut";
-import Navbar from "../common/components/Navbar";
-import SearchForm from "../common/components/SearchForm";
+import Card from "../common/components/card/Card";
+import Header from "../common/components/Header";
 
 export default function MainPage() {
-
-  
-
-
   return (
     <>
-      <Navbar></Navbar>
-      
-      <SearchForm></SearchForm>
+      <Header></Header>
+      <Card></Card>
     </>
-  )
+  );
 }
