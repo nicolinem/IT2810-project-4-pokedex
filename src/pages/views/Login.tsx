1e9fe50604684452c83046636bd9fe60bbb1e84b
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-10-31 18:34:27 +0100

    style: oriented on right side of page

--- src/pages/views/Login.tsx
+++ src/pages/views/Login.tsx
@@ -1,18 +1,8 @@
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Avatar from "@mui/material/Avatar";
-import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
-import Container from "@mui/material/Container";
-import CssBaseline from "@mui/material/CssBaseline";
-import Grid from "@mui/material/Grid";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import TextField from "@mui/material/TextField";
-import Typography from "@mui/material/Typography";
-import * as React from "react";
 import { Link, useNavigate } from "react-router-dom";
+import Button from "../../common/components/button/Button";
 import { useLogin } from "../../common/hooks/useLogin";
 
-const theme = createTheme();
 
 
 type Props = {
@@ -51,8 +41,8 @@ export default function LoginPage( props : Props) {
   };
 
   return (
-    <div id="authentication-modal"  aria-hidden="true" className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
-    <div className="relative p-4 w-full max-w-md h-full md:h-auto">
+    <div id="authentication-modal"  aria-hidden="true" className="text-gray-900 overflow-y-auto overflow-x-hidden fixed top-0 right-0 z-50 w-full md:inset-0 h-modal md:h-full">
+    <div className="absolute p-4 w-full right-0 max-w-md h-full md:h-auto">
         <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button onClick={props.onExit} type="button" className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white">
                 <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -69,12 +59,13 @@ export default function LoginPage( props : Props) {
                         <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your password</label>
                         <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required/>
                     </div>
-                    <button type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login to your account</button>
-                    <button onClick={props.onSignUp}>signup</button>
-              {/* <div  className="text-sm font-medium text-gray-500 dark:text-gray-300">
-                        Not registered? <a  className="text-blue-700 hover:underline dark:text-blue-500">Create account</a>
-                    </div> */}
-                </form>
+              <Button buttonType="primary"  type={"submit"}>Sign in</Button>
+                    
+            </form>
+            <div className="mt-5">
+              Not registered? <a onClick={props.onSignUp}className="text-blue-700 hover:underline dark:text-blue-500 hover:cursor-pointer">Create account</a>
+            </div>
+            
             </div>
         </div>
     </div>
