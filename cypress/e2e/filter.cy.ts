91f9d9e243271e3e148a674f0e897237464c8050
elizabap <elizabap@stud.ntnu.no>
2022-11-01 16:35:49 +0100

    feat: filter

--- cypress/e2e/filter.cy.ts
+++ cypress/e2e/filter.cy.ts
@@ -1,5 +1,17 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import {} from "cypress";
+
+describe("Visit HomePage", () => {
+  it("Check homepage url", () => {
+    cy.visit("localhost:3000");
+  });
+});
+
+describe("Open advanced search", () => {
+  it("Check if advanced search works", () => {
+    cy.get("button").contains("Advanced Search").click();
+  });
+  it("Check if type works", () => {
+    cy.get("button").contains("fire").click();
+    cy.get("div[card]").contains("fire");
+  });
+});
