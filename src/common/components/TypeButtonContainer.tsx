84b57ce9560c05e03df864af1ed7af571dedd0f1
elizabap <elizabap@stud.ntnu.no>
2022-11-01 16:41:06 +0100

    fix: type within container

--- src/common/components/TypeButtonContainer.tsx
+++ src/common/components/TypeButtonContainer.tsx
@@ -1,68 +1,64 @@
-import TypeButton from "./button/TypeButton";
-import { matchType } from "../../types/pokemon.utils";
 import { useState } from "react";
+import { matchType } from "../../types/pokemon.utils";
+import TypeButton from "./button/TypeButton";
 
 type props = {
-    getActiveTypes: (activeTypes: string[]) => void;
-}
+  getActiveTypes: (activeTypes: string[]) => void;
+};
 
-export const TypeButtonContainer: React.FC<props> = ({getActiveTypes}) => {
+export const TypeButtonContainer: React.FC<props> = ({ getActiveTypes }) => {
+  const [activeTypes, setactiveTypes] = useState<string[]>([]);
 
-    const [activeTypes, setactiveTypes] = useState<string[]>([]);
+  const types: string[] = [
+    "bug",
+    "dark",
+    "dragon",
+    "electric",
+    "fairy",
+    "fighting",
+    "fire",
+    "flying",
+    "ghost",
+    "ground",
+    "ice",
+    "normal",
+    "poison",
+    "psychic",
+    "rock",
+    "steel",
+    "water",
+    "grass",
+  ];
 
-    const types: string[] = [
-        "bug",
-        "dark",
-        "dragon",
-        "electric",
-        "fairy",
-        "fighting",
-        "fire",
-        "flying",
-        "ghost",
-        "ground",
-        "ice",
-        "normal",
-        "poison",
-        "psychic",
-        "rock",
-        "steel",
-        "water",
-        "grass"
-    ];
+  const activateButton = (type: string): void => {
+    var currentTypes: string[] = activeTypes;
 
-    
+    if (currentTypes.includes(type)) {
+      const index = currentTypes.indexOf(type);
+      currentTypes.splice(index, 1);
+      setactiveTypes(currentTypes);
+    } else {
+      currentTypes.push(type);
+      setactiveTypes(currentTypes);
+    }
 
-    const activateButton = (type: string): void => {
-        var currentTypes: string[] = activeTypes;
+    getActiveTypes(activeTypes);
+  };
 
-        if (currentTypes.includes(type)) {
-            const index = currentTypes.indexOf(type)
-            currentTypes.splice(index,1)
-            setactiveTypes(currentTypes)
-        } else{
-            currentTypes.push(type);
-            setactiveTypes(currentTypes);
-        }
+  const typeButtons = (types: string[]) => {
+    return types.map((type: string) => (
+      <TypeButton
+        type={matchType(type)}
+        activateButton={activateButton}
+      ></TypeButton>
+    ));
+  };
 
-        getActiveTypes(activeTypes);
-    };
-
-    
-
-    const typeButtons = (types: string[]) => {
-        return(
-        types.map((type: string) => (
-            <TypeButton type={matchType(type)} activateButton={activateButton}></TypeButton>
-        ))
-    );};
-
-return (
+  return (
     <div className="border-2 border-white rounded-xl">
-        <div className="flex flex-wrap justify-center gap-4 m-8 w-11/12">
-            {typeButtons(types)}
-        </div>
+      <div className="flex flex-wrap justify-center gap-4 m-8 w-12/12">
+        {typeButtons(types)}
+      </div>
     </div>
-)
-
-}
+  );
+};
