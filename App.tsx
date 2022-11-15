113f985611129b776e1b795c5f2660f0a6223d05
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-11-15 12:51:21 +0100

    cleanup

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { ApolloClient, ApolloProvider, gql, InMemoryCache, useQuery } from '@apollo/client';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, AppRegistry, Button, Pressable } from 'react-native';
-import { Testquery } from "./Testquery"
+import { PokemonPage } from "./src/components/pokemon/PokemonPage"
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import SearchPage from './src/components/searchpage/SearchPage';
@@ -16,65 +16,39 @@ export default function App() {
     cache: new InMemoryCache()
 });
 
-type RootStackParamList = {
-  Home: undefined;
-  Pokemon: { pokemonID: number };
-  Feed: { sort: 'latest' | 'top' } | undefined;
-};
   
 
   return (
-    <ApolloProvider client={client}>
-      <NavigationContainer>
-        <Stack.Navigator screenOptions={({ navigation }) => ({
-              headerStyle: {
-              backgroundColor: "#121A36",
-               
-              },
-              headerTintColor: "#fff",
-            })}>
-           <Stack.Screen
-          name="Home"
-          component={SearchPage}
-            options={{
-              title: 'P o k è d e x',
-              }}
-        />
-           <Stack.Screen
-          name="Pokemon"
-          component={Testquery}
-          options={{ title: '' }}
-        />
-       
-          </Stack.Navigator>
-      </NavigationContainer>
-      <StatusBar></StatusBar>
+      <ApolloProvider client={client}>
+        <NavigationContainer>
+          <Stack.Navigator screenOptions={() => ({
+                headerStyle: {
+                backgroundColor: "#121A36",
+                
+                  },
+                  headerTintColor: "#fff",
+                })}>
+              <Stack.Screen
+              name="Home"
+              component={SearchPage}
+                options={{
+                  title: 'P o k è d e x',
+                  }}
+            />
+              <Stack.Screen
+              name="Pokemon"
+              component={PokemonPage}
+              options={{ title: '' }}
+            />
+          
+              </Stack.Navigator>
+          </NavigationContainer>
+        <StatusBar></StatusBar>
       </ApolloProvider>
   );
 }
 
-const HomeScreen = () => {
-  return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() =>
-        console.log("pressed")
-      }
-    />
-  );
-};
-const ProfileScreen = (navigation: {navigate(path: string): void}) => {
-  return <Text>This iss profile</Text>;
-};
 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 AppRegistry.registerComponent('MyApplication', () => App);
