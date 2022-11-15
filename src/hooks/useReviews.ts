180f3bba67ce9678362f92dc6f62b99c58af39ab
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-11-15 07:11:24 +0100

    implement

--- src/hooks/useReviews.ts
+++ src/hooks/useReviews.ts
@@ -1,31 +1,32 @@
-// import { useQuery, gql } from '@apollo/client';
-// // import { useParams } from 'react-router-dom';
-// import useProfile from './useProfile';
+import { useQuery, gql } from '@apollo/client';
+import { useRoute } from '@react-navigation/native';
+import { PokemonScreenRouteProp } from '../types/navigation.types';
+import useProfile from './useProfile';
 
 
-// const REWIEWLIST_QUERY = gql`
-//   query ($pokemonID: Int){
-//     reviews(pokemonID: $pokemonID) {
-//       content
-//       score
-//       date
-//       user
-//     }
-//   }
-// `;
+const REWIEWLIST_QUERY = gql`
+  query ($pokemonID: Int){
+    reviews(pokemonID: $pokemonID) {
+      content
+      score
+      date
+      user
+    }
+  }
+`;
 
-// export default function useReviews() {
-//   // const { id } = useParams<{ id: string }>();
-//   const pokemonID = parseInt(id!);
+export default function useReviews() {
+    const route = useRoute<PokemonScreenRouteProp>();
+    const { id } = route.params;
 
-//   const { data, loading, error, refetch } = useQuery(REWIEWLIST_QUERY, { variables: { pokemonID } });
+
+  const { data, loading, error, refetch } = useQuery(REWIEWLIST_QUERY, { variables: { pokemonID: id } });
 //   const profile = useProfile();
-
-//     return {
-//         reviews: data?.reviews || ["whut"],
-//         refetchReviews: () => {
-//             refetch();
-//             if (profile.refetch) profile.refetch();
-//         },
-//     }
-// // }
+    return {
+        reviews: data?.reviews || ["whut"],
+        refetchReviews: () => {
+            refetch();
+            // if (profile.refetch) profile.refetch();
+        },
+    }
+}
