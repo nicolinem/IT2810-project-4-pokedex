c212fbefa4c6caabe8291cbacf4135bdd69fb70d
Nicoline Mork <69582554+nicolinem@users.noreply.github.com>
2022-11-22 15:59:55 +0100

    fix: remove log

--- src/components/searchpage/SearchPage.tsx
+++ src/components/searchpage/SearchPage.tsx
@@ -1,4 +1,4 @@
-import React, { SetStateAction, useState } from "react";
+import React, { SetStateAction, useEffect, useState } from "react";
 import SearchResult from "./SearchResult";
 import { View, TextInput, Text, Button } from 'react-native';
 import tw from 'twrnc';
@@ -6,26 +6,24 @@ import { TypeButtonContainer } from "./TypeButtonContainer";
 import { Accordion } from "./Accordion";
 import { SelectList } from "react-native-dropdown-select-list";
 import { sortData } from "../../utils/constants";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const SearchPage = () => {
   const [searchText, setSearchText] = useState("");
   const [activeTypes, setActiveTypes] = useState<String[]>([]);
   const [offset, setOffset] = useState(0);
   const [sort, setSort] = useState<string>("ASC");
-
   
-
+ 
   
   const getActiveTypes = (activatedTypes: string[]) => {
     const newactivatedTypes: string[] = [...activatedTypes];
     setActiveTypes(newactivatedTypes);
   };
   
-  console.log(offset + " active types: " + activeTypes)
   return (
     <View style={tw`flex-1`}>
 
-      
       
       <View style={tw`bg-[#121A36] w-full flex items-center justify-center pt-1`}>
 
@@ -33,7 +31,7 @@ const SearchPage = () => {
           <TextInput style={tw`bg-[#3F4867] text-[#FFFFFF] rounded-full w-2/3 h-9 pl-5`} placeholder={"Søk"} value={searchText}
                     onChangeText={(text: string) => { setSearchText(text) }} />
         
-        <View style={tw`w-full`}>
+        <View style={tw`w-full pb-2`}>
           <Accordion title={"Filter search"} content={ <View>
             <TypeButtonContainer getActiveTypes={getActiveTypes}></TypeButtonContainer>
 
